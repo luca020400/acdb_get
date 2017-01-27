f8e23cf31aaae409407c951979dad23a2d2af923
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-01-27 19:47:18 +0100

    Cleanup error handling

--- acdb_get.c
+++ acdb_get.c
@@ -7,23 +7,19 @@ typedef int (*platform_get_snd_device_acdb_id_t)(int);
 
 int main() {
     void *handle;
-    const char *error;
     platform_get_snd_device_acdb_id_t platform_get_snd_device_acdb_id;
 
     handle = dlopen(LIB_AUDIO_HAL, RTLD_LAZY);
     if (!handle) {
         fprintf(stderr, "%s\n", dlerror());
-        exit(EXIT_FAILURE);
+        return 1;
     }
 
-    dlerror(); /* Clear any existing error */
+    platform_get_snd_device_acdb_id = (platform_get_snd_device_acdb_id_t) dlsym(handle, "platform_get_snd_device_acdb_id");
 
-    platform_get_snd_device_acdb_id = (platform_get_snd_device_acdb_id_t)dlsym(handle, "platform_get_snd_device_acdb_id");
-
-    error = dlerror();
-    if (error != NULL)  {
-        fprintf(stderr, "%s\n", error);
-        exit(EXIT_FAILURE);
+    if (!platform_get_snd_device_acdb_id)  {
+        fprintf(stderr, "%s\n", dlerror());
+        return 1;
     }
 
     printf("    <acdb_ids>\n");
@@ -32,5 +28,5 @@ int main() {
     printf("    </acdb_ids>\n");
 
     dlclose(handle);
-    exit(EXIT_SUCCESS);
+    return 0;
 }
